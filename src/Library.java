d1d3f12e03794031cba69304f9f5066448d51c0c
“SyedAliMasoodBukhari” <syedali.masood@icloud.com>
2024-10-04 19:48:46 +0500

    add rem book func

--- src/Library.java
+++ src/Library.java
@@ -1,15 +1,24 @@
 import java.util.ArrayList;
-   import java.util.List;
+import java.util.List;
 
-   public class Library {
-       private List<String> books = new ArrayList<>();
+public class Library {
+	private List<String> books = new ArrayList<>();
 
-       public void addBook(String bookName) {
-           books.add(bookName);
-           System.out.println(bookName + " added to library.");
-       }
+	public void addBook(String bookName) {
+		books.add(bookName);
+		System.out.println(bookName + " added to library.");
+	}
 
-       public void listBooks() {
-           System.out.println("Books in library: " + books);
-       }   }
- 
+	public void listBooks() {
+		System.out.println("Books in library: " + books);
+	}
+
+	public void removeBook(String bookName) {
+		 if (books.remove(bookName)) {
+			 System.out.println(bookName + " removed from library."); 
+		 } else {
+			 System.out.println(bookName + " not found in library.");
+		 }
+
+	}
+}
